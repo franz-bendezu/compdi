482de265b27d1cb95e6626269bc7eea01260eaa9
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2026-05-09 15:48:41 +0000

    feat: add @compdi/core package with type-safe DI macros

---
+++ packages/core/src/index.ts
@@ -0,0 +1,51 @@
+export type Constructor<T = object> = new (...args: any[]) => T;
+export type AsyncFactory<T> = () => Promise<T>;
+
+function macroNotTransformed(name: string): never {
+  throw new Error(
+    `[compdi] Runtime call to ${name}. Configure unplugin-compdi to erase macro calls during build.`
+  );
+}
+
+export function defineSingleton<TCtor extends Constructor>(
+  target: TCtor,
+  deps: readonly unknown[]
+): InstanceType<TCtor> {
+  void target;
+  void deps;
+  return macroNotTransformed("defineSingleton");
+}
+
+export function defineTransient<TCtor extends Constructor>(
+  target: TCtor,
+  deps: readonly unknown[]
+): () => InstanceType<TCtor> {
+  void target;
+  void deps;
+  return macroNotTransformed("defineTransient");
+}
+
+export function defineLazySingleton<TCtor extends Constructor>(
+  target: TCtor,
+  deps: readonly unknown[]
+): InstanceType<TCtor> {
+  void target;
+  void deps;
+  return macroNotTransformed("defineLazySingleton");
+}
+
+export function defineAsyncSingleton<TFactory extends AsyncFactory<any>>(
+  factory: TFactory,
+  deps: readonly unknown[]
+): Awaited<ReturnType<TFactory>> {
+  void factory;
+  void deps;
+  return macroNotTransformed("defineAsyncSingleton");
+}
+
+export function defineAppTeardown(
+  resources: readonly unknown[]
+): () => Promise<void> {
+  void resources;
+  return macroNotTransformed("defineAppTeardown");
+}
