ce1015799eccad5f43de074e2c3103c6910ae43e
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2026-05-09 15:48:52 +0000

    feat: add unplugin-compdi with OXC-powered macro transformations

---
+++ packages/unplugin/src/index.ts
@@ -0,0 +1,45 @@
+import { createUnplugin } from "unplugin";
+import { transformCompdiMacros } from "./transform";
+
+export type CompdiPluginOptions = {
+  include?: RegExp;
+};
+
+export type CompdiPluginFactory = (options?: CompdiPluginOptions) => unknown;
+export type CompdiUnplugin = {
+  vite: CompdiPluginFactory;
+  rollup: CompdiPluginFactory;
+};
+
+const defaultInclude = /\.[cm]?[jt]sx?$/;
+
+const compdiPlugin = createUnplugin<CompdiPluginOptions>((options) => ({
+  name: "unplugin-compdi",
+  enforce: "pre",
+  transform(code, id) {
+    const include = options?.include ?? defaultInclude;
+
+    if (!include.test(id)) {
+      return null;
+    }
+
+    const transformed = transformCompdiMacros(code, id);
+    if (!transformed) {
+      return null;
+    }
+
+    return {
+      code: transformed,
+      map: null
+    };
+  }
+}));
+
+export const compdi: CompdiUnplugin = {
+  vite: (options = {}) => compdiPlugin.vite(options),
+  rollup: (options = {}) => compdiPlugin.rollup(options)
+};
+
+export default compdi;
+export const vite = compdi.vite;
+export const rollup = compdi.rollup;
