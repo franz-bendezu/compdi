985b4b984499dcc136d2e95589f3e4cf2c7a1f52
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2026-05-11 14:10:23 +0000

    feat: add rolldown support and enhance binding resolution in transformCompdiMacros

--- packages/unplugin/src/index.ts
+++ packages/unplugin/src/index.ts
@@ -9,6 +9,7 @@ export type CompdiPluginFactory = (options?: CompdiPluginOptions) => unknown;
 export type CompdiUnplugin = {
   vite: CompdiPluginFactory;
   rollup: CompdiPluginFactory;
+  rolldown: CompdiPluginFactory;
 };
 
 const defaultInclude = /\.[cm]?[jt]sx?$/;
@@ -37,9 +38,11 @@ const compdiPlugin = createUnplugin<CompdiPluginOptions>((options) => ({
 
 export const compdi: CompdiUnplugin = {
   vite: (options = {}) => compdiPlugin.vite(options),
-  rollup: (options = {}) => compdiPlugin.rollup(options)
+  rollup: (options = {}) => compdiPlugin.rollup(options),
+  rolldown: (options = {}) => compdiPlugin.rolldown(options),
 };
 
 export default compdi;
 export const vite = compdi.vite;
 export const rollup = compdi.rollup;
+export const rolldown = compdi.rolldown;
